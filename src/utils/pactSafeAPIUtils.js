52bfa64384e57c89ed6e1ea2d6be5575c7366d5a
Doug Do <1464176+DouglasDDo@users.noreply.github.com>
2021-04-30 08:18:18 -0700

    Adds contract fetch. Mostly working but groupID hardcoded in.

--- src/utils/pactSafeAPIUtils.js
+++ src/utils/pactSafeAPIUtils.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
 import HE from 'he'
 
-// placeholder for API tokens
 import {PS_AUTH_TOKEN, PS_ACCESS_ID, PS_GROUP_ID} from '@env'
 
 const psGroupEndpoint = 'https://pactsafe.io/load/json?sid=' + PS_ACCESS_ID;
 
-export const getContractGroupByID = async (group_key, setPsGroup) => {
+export const getContractGroupByID = async (group_key, setGroup) => {
     const url = psGroupEndpoint + '&gkey=' + group_key;
 
     try {
         await axios.get(url, { headers: { 'Authorization': `Bearer ${PS_ACCESS_ID}` } })
         .then( (response) => {
-            setPsGroup({ value: response.data, error: 'Group Fetch Error' });
+            setGroup({ value: response.data, error: 'Group Fetch Error' });
             console.log(response.data) 
         });
     } catch(error) {
@@ -20,6 +19,21 @@ export const getContractGroupByID = async (group_key, setPsGroup) => {
     }
 };
 
+export const getLatestPublishedContractById = async (contract_id, setContract) => {
+    // const url = 'https://api.pactsafe.com/v1.1/contracts/' + PS_GROUP_ID + '/versions/@published'
+    const url = 'https://api.pactsafe.com/v1.1/contracts/' + 117616 + '/versions/@published'
+
+    try {
+        await axios.get(url)
+        .then( (response) => {
+            setContract({value: HE.unescape(response.data.data.body), error: ''});
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// export const getContractById = async () => {};
 
 
 export const getLatestPublishedContractById;
